8a72022d078e19d99a7c992f6f7e13b06f8f4a1e
JonathanThing <jonathancai834@gmail.com>
2022-04-04 22:01:49 -0400

    fixed weird thing

--- client/src/pages/home/components/TeamMemberCard.tsx
+++ client/src/pages/home/components/TeamMemberCard.tsx
@@ -9,7 +9,6 @@ const FlipCardInner = styled.div`
   text-align: center;
   transition: transform 0.6s;
   transform-style: preserve-3d;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 `;
 
 const FlipCardFace = styled.div`
